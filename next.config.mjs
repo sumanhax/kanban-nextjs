55d30c8f21ced272ba0dc08e3a8b4fcbf6d07ca8
sumanhax <suman.hax@gmail.com>
2025-08-20 13:40:42 +0530

    cors

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,21 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+async headers() {
+    return [
+      {
+        // Apply CORS headers to all routes
+        source: "/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-Requested-With, Content-Type, Authorization" },
+        ],
+      },
+    ];
+  },
+
 }
 
 export default nextConfig
